6f6c8448f4c5a4b95671ea6dc563dbd2d6b10e1a
oliveira-renato <renato.printf@gmail.com>
2022-07-03 22:11:55 -0300

    creating new class: AuctionsListing

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,18 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class AuctionsListing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=2000)
+    inical_bid = models.FloatField()
+    image_url = models.URLField()
+    active = models.BooleanField()
+    
+    def __str__(self):
+        return f"{self.title} {self.description} ${self.inical_bid} {self.active}"
+class Bids():
+    pass
+
+class Comments():
+    pass
