f9027bed47b00079d23379aa4466ab5e418f8916
oliveira-renato <renato.printf@gmail.com>
2022-07-03 22:30:37 -0300

    fixing model

--- auctions/models.py
+++ auctions/models.py
@@ -6,12 +6,13 @@ class User(AbstractUser):
     pass
 
 class AuctionsListing(models.Model):
-    title = models.CharField(max_length=64)
-    description = models.CharField(max_length=2000)
-    inical_bid = models.FloatField()
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=64, blank=False, null=False)
+    description = models.CharField(max_length=2000, blank=False, null=False)
+    inicial_bid = models.FloatField()
     image_url = models.URLField()
     active = models.BooleanField()
-    
+
     def __str__(self):
         return f"{self.title} {self.description} ${self.inical_bid} {self.active}"
 class Bids():
