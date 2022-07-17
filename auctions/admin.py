ca8641702703b1b1932790dba19d17a01c4332b2
oliveira-renato <renato.printf@gmail.com>
2022-07-17 18:28:19 -0300

    fix: categories and add more itens.

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import AuctionsListing, User 
+from .models import AuctionsListing, User, Category 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','name')
 class AuctionsListingAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'inicial_bid', 'active')
 
@@ -11,3 +13,4 @@ class UsersAdmin(admin.ModelAdmin):
 
 admin.site.register(AuctionsListing, AuctionsListingAdmin)
 admin.site.register(User, UsersAdmin)
+admin.site.register(Category, CategoryAdmin)
--- auctions/models.py
+++ auctions/models.py
@@ -2,7 +2,12 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
 class User(AbstractUser):
     id = models.AutoField(primary_key=True)
     
@@ -16,6 +21,7 @@ class AuctionsListing(models.Model):
     null=False)
     inicial_bid = models.FloatField()
     image_url = models.URLField(max_length=6000)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,default=None,related_name='categories', blank=True, null=True)
     active = models.BooleanField()
 
     def __str__(self):
