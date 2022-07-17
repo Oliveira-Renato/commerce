c70693b2a6470522d2d6eb0f83ed890b5e9dd710
oliveira-renato <renato.printf@gmail.com>
2022-07-17 18:43:46 -0300

    merge sucefully

--- auctions/models.py
+++ auctions/models.py
@@ -26,14 +26,6 @@ class AuctionsListing(models.Model):
 
     def __str__(self):
         return f"title:{self.title} desc:{self.description} ${self.inicial_bid} img:{self.image_url} active:{self.active}"
-
-class Category(models.Model):
-    id = models.AutoField(primary_key=True)
-    auction = models.ManyToManyField(AuctionsListing,default=None,related_name='categories')
-    name = models.CharField(max_length=64, blank=False, null=False)
-
-    def __str__(self):
-        return f"{self.name}"
 class Bids():
     pass
 
