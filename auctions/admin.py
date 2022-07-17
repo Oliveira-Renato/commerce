f89f02e8b687a2df8bbf5aa6fe6b90e758f9fd69
oliveira-renato <renato.printf@gmail.com>
2022-07-16 23:12:09 -0300

    feat: category created

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import AuctionsListing, User 
+from .models import AuctionsListing, User, Category 
 
 class AuctionsListingAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'inicial_bid', 'active')
@@ -9,5 +9,9 @@ class AuctionsListingAdmin(admin.ModelAdmin):
 class UsersAdmin(admin.ModelAdmin):
     list_display = ('id','username', 'email')
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','name')
+
 admin.site.register(AuctionsListing, AuctionsListingAdmin)
 admin.site.register(User, UsersAdmin)
+admin.site.register(Category, CategoryAdmin)
