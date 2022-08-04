0b4bba4ac315205e91ffb9d1bc0897b99d8c5c18
Oliveira Renato <renato.printf@gmail.com>
2022-08-04 15:04:34 -0300

    procfile back

--- commerce/settings.py
+++ commerce/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 from decouple import config
 import os
 
-from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,4 +125,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
