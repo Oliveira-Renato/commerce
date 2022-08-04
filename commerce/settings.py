f9ab9f733cd487d117733d106459d8b7e3cd41ff
Oliveira Renato <renato.printf@gmail.com>
2022-08-04 12:40:00 -0300

    hosting on Heroku 4

--- commerce/settings.py
+++ commerce/settings.py
@@ -25,6 +25,8 @@ SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+DEBUG_COLLECTSTATIC=1
+
 ALLOWED_HOSTS = ['127.0.0.1', 'auctionslisting.herokuapp.com']
 
 
@@ -121,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
