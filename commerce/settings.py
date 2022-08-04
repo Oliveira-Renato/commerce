733ae45a23c6fbcb2b4aec551b75a29e6d1ac097
Oliveira Renato <renato.printf@gmail.com>
2022-08-04 14:42:59 -0300

    HEROKU deploy 14

--- commerce/settings.py
+++ commerce/settings.py
@@ -15,7 +15,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
