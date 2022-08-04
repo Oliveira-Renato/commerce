a7f8ce281a1afe0aee7f553257a2a5056d089fcd
Oliveira Renato <renato.printf@gmail.com>
2022-08-04 13:05:49 -0300

    HEROKU deploy 8

--- commerce/urls.py
+++ commerce/urls.py
@@ -18,6 +18,9 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("auctions.urls")),
