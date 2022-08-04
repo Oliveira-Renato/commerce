fe5bc638482291ab0de6f3d359f415ea3eb3db58
Oliveira Renato <renato.printf@gmail.com>
2022-08-04 12:43:35 -0300

    hosting on Heroku 5

--- commerce/urls.py
+++ commerce/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
