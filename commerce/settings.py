de917676ac2ffe896320826e4b2de758394ddfcc
Renato  Oliveira <81115873+Oliveira-Renato@users.noreply.github.com>
2025-03-21 18:18:42 +0000

    teste

--- commerce/settings.py
+++ commerce/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/auctions/staticfiles/')
+    os.path.join(BASE_DIR, '/auctions/static/')
 ]
 
 
